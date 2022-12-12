2fb9cdca89a45f384b965dc6f561c2806495c176
Nico <78444127+Nipicoco@users.noreply.github.com>
2022-12-12 00:09:10 -0300

    updated

--- C-POINTERS/main.c
+++ C-POINTERS/main.c
@@ -4,39 +4,39 @@ int main()
     int num1, num2, num3;
     int *p1, *p2, *p3;
 
-    //Input usuario
-    printf("Ingresar Primer Numero: ");
-    scanf("%d",&num1);
-    printf("Ingresar Segundo Numero: ");
-    scanf("%d",&num2);
-    printf("Ingresar Tercer Numero: ");
-    scanf("%d",&num3);
+    // Input usuario
+    printf("Enter First number: ");
+    scanf("%d", &num1);
+    printf("Enter Second number: ");
+    scanf("%d", &num2);
+    printf("Enter Third number: ");
+    scanf("%d", &num3);
 
-    //Asignacion de numeros a los ppointers
-    p1 = &num1;
-    p2 = &num2;
-    p3 = &num3;
-    if(*p1 > *p2)
-    {
-	if(*p1 > *p3)
-	{
-		printf("%d es el numero mas grande", *p1);
-	}
-	else
-	{
-		printf("%d es el numero mas grande", *p3);
-	}
+    // Assign pointers to the numbers
+    p1 = &num1;        // p1 points to num1
+    p2 = &num2;        // p2 points to num2
+    p3 = &num3;        // p3 points to num3
+    if (*p1 > *p2)     // if num1 is greater than num2
+    {                  // then check if num1 is greater than num3
+        if (*p1 > *p3) // if num1 is greater than num3
+        {
+            printf("%d is the biggest number", *p1); // then num1 is the biggest number
+        }
+        else
+        { // if num3 is greater than num1
+            printf("%d is the biggest number", *p3);
+        }
     }
     else
     {
-	if(*p2 > *p3)
-	{
-		printf("%d es el numero mas grande", *p2);
-	}
-	else
-	{
-		printf("%d es el numero mas grande", *p3);
-	}
+        if (*p2 > *p3)
+        {
+            printf("%d is the biggest number", *p2); // then num2 is the biggest number
+        }
+        else
+        {
+            printf("%d is the biggest number", *p3);
+        } // if num3 is greater than num2
     }
     return 0;
 }
